de90524fc868498e281144444ea9cf90dfee3b34
MichalTrojek <MichalTrojek1@gmail.com>
2019-09-30 21:56:58 +0200

    refactoring\

--- app/src/main/java/cz/mtr/analyzaprodeju/fragments/ftp/storedata/DownloadStoreDataFtpTask.java
+++ app/src/main/java/cz/mtr/analyzaprodeju/fragments/ftp/storedata/DownloadStoreDataFtpTask.java
@@ -3,22 +3,11 @@ package cz.mtr.analyzaprodeju.fragments.ftp.storedata;
 import android.os.AsyncTask;
 import android.util.Log;
 
-import com.opencsv.CSVParser;
-import com.opencsv.CSVParserBuilder;
-import com.opencsv.CSVReader;
-import com.opencsv.CSVReaderBuilder;
-
 import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-
 import cz.mtr.analyzaprodeju.models.Model;
-import cz.mtr.analyzaprodeju.models.datastructures.StoreItem;
 
 public class DownloadStoreDataFtpTask extends AsyncTask<String, Integer, Boolean> {
 
@@ -58,7 +47,7 @@ public class DownloadStoreDataFtpTask extends AsyncTask<String, Integer, Boolean
                 for (FTPFile f : ftp.listFiles()) {
                     Log.d(TAG, mFilename);
                     if (f.getName().toLowerCase().equals(mFilename.toLowerCase())) {
-                        Model.getInstance().setStoreItems(readStoreStatus(ftp.retrieveFileStream(mFilename)));
+                        Model.getInstance().setStoreItems(new StoreDataReader().getStoreStatus(ftp.retrieveFileStream(mFilename)));
                         break;
                     }
                 }
@@ -76,41 +65,6 @@ public class DownloadStoreDataFtpTask extends AsyncTask<String, Integer, Boolean
     }
  
 
-    private HashMap<String, StoreItem> readStoreStatus(InputStream input) {
-        HashMap<String, StoreItem> items = new HashMap<>();
-        try {
-            CSVParser parser = new CSVParserBuilder().withSeparator(';').withIgnoreQuotations(true).build();
-            CSVReader reader = new CSVReaderBuilder(new InputStreamReader(input, "Windows-1250")).withSkipLines(1).withCSVParser(parser).build();
-            String[] record;
-            while ((record = reader.readNext()) != null) {
-
-                try {
-
-                    if (items.containsKey(record[0].trim().toLowerCase())) {
-                        StoreItem itemInMap = items.get(record[0]);
-                        int totalAmount = Integer.parseInt(itemInMap.getAmount()) + Integer.parseInt(record[8]);
-                        String regal = itemInMap.getLocation() + " " + record[3];
-                        items.get(record[0]).setAmount(totalAmount + "");
-                        items.get(record[0]).setLocation(regal);
-                    } else {
-                        items.put(record[0].trim().toLowerCase(), new StoreItem(record[0], record[3], record[8], record[2], record[4]));
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    continue;
-                }
-            }
-            if (items.size() != 0) {
-                Log.d(TAG, "Store Items " + items.size() + " Velikost");
-
-            }
-            reader.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return items;
-    }
 
     @Override
     protected void onPostExecute(Boolean success) {
--- app/src/main/java/cz/mtr/analyzaprodeju/services/asynctasks/UpdateStoreDataTask.java
+++ app/src/main/java/cz/mtr/analyzaprodeju/services/asynctasks/UpdateStoreDataTask.java
@@ -1,24 +1,13 @@
 package cz.mtr.analyzaprodeju.services.asynctasks;
 
 import android.os.AsyncTask;
-import android.util.Log;
-
-import com.opencsv.CSVParser;
-import com.opencsv.CSVParserBuilder;
-import com.opencsv.CSVReader;
-import com.opencsv.CSVReaderBuilder;
 
 import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-
+import cz.mtr.analyzaprodeju.fragments.ftp.storedata.StoreDataReader;
 import cz.mtr.analyzaprodeju.models.Model;
-import cz.mtr.analyzaprodeju.models.datastructures.StoreItem;
 import cz.mtr.analyzaprodeju.repository.preferences.GeneralPreferences;
 
 public class UpdateStoreDataTask extends AsyncTask<String, Integer, Boolean> {
@@ -62,7 +51,7 @@ public class UpdateStoreDataTask extends AsyncTask<String, Integer, Boolean> {
                     }
                     if (f.getName().toLowerCase().equals(mFilename.toLowerCase())) {
                         GeneralPreferences.getInstance().saveLastStoreNameFile(f.getName().toLowerCase());
-                        Model.getInstance().setStoreItems(readStoreStatus(ftp.retrieveFileStream(mFilename)));
+                        Model.getInstance().setStoreItems(new StoreDataReader().getStoreStatus(ftp.retrieveFileStream(mFilename)));
                         break;
                     }
                 }
@@ -77,45 +66,6 @@ public class UpdateStoreDataTask extends AsyncTask<String, Integer, Boolean> {
             e.printStackTrace();
         }
         return null;
-    }
-
-
-
-    private HashMap<String, StoreItem> readStoreStatus(InputStream input) {
-        HashMap<String, StoreItem> items = new HashMap<>();
-        Log.d(TAG, "Service Store Update started");
-        try {
-            CSVParser parser = new CSVParserBuilder().withSeparator(';').withIgnoreQuotations(true).build();
-            CSVReader reader = new CSVReaderBuilder(new InputStreamReader(input, "Windows-1250")).withSkipLines(1).withCSVParser(parser).build();
-            String[] record;
-            while ((record = reader.readNext()) != null) {
-
-                try {
-
-                    if (items.containsKey(record[0].trim().toLowerCase())) {
-                        StoreItem itemInMap = items.get(record[0]);
-                        int totalAmount = Integer.parseInt(itemInMap.getAmount()) + Integer.parseInt(record[8]);
-                        String regal = itemInMap.getLocation() + " " + record[3];
-                        items.get(record[0]).setAmount(totalAmount + "");
-                        items.get(record[0]).setLocation(regal);
-                    } else {
-                        items.put(record[0].trim().toLowerCase(), new StoreItem(record[0], record[3], record[8], record[2], record[4]));
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    continue;
-                }
-            }
-            if (items.size() != 0) {
-                Log.d(TAG, "Service Store Items " + items.size() + " Velikost");
-
-            }
-            reader.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return items;
     }
 
 
